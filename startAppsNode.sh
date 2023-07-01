41e23bc3d1fa710b2db6fbddc0b608c1bbb9fa56
PeKaStLa <14peterstadler@gmail.com>
2023-07-01 14:36:56 +0000

    finale kleine Änderungen

--- startAppsNode.sh
+++ startAppsNode.sh
@@ -7,8 +7,8 @@
 #    script for automating restart for all npm/node apps        #
 #tasks:                                                         #
 #1. Loop over repo-names and ports                              #
-#2. check for free port                                         #
-#3. Deploy them                                                 #
+#   2. check for free port                                      #
+#   3. Deploy them                                              #
 #################################################################
 #
 
@@ -19,8 +19,8 @@ declare  -r ports=(3010 3011);
 
 for ((i=0; i<${#repos[@]}; i++))
 do
-    echo "Now check and deploy ${repos[i]} if not running on port ${ports[i]}";
-    IsPortFree ${ports[i]}  && TmuxDeployNode ${repos[i]} ${ports[i]};
+    echo "------Now check and deploy ${repos[i]} if not running on port ${ports[i]}";
+    IsPortFree ${ports[i]} && TmuxDeployNode ${repos[i]} ${ports[i]};
 done
 
 
